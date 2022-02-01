3a0bffb28776d280fddc910666ee3732282f34f1
laurentmichel <laurentmichel@localhost>
2022-02-01 16:59:36 +0100

    comment code

--- examples/modelmapping/test.7.votable_pointer.py
+++ examples/modelmapping/test.7.votable_pointer.py
@@ -9,6 +9,14 @@ from astropy.io.votable import parse
 from astropy.io.votable.tree import VOTableFile, Resource, ModelMapping
 
 data_path = os.path.dirname(os.path.realpath(__file__))
+
+"""
+Read out Test
+"""
+
+# Read a valid VOTable
+# No invalid element detected,
+# The block is returned as an XML String
 vpath = os.path.join(data_path, "test.7.xml")
 votable = parse(vpath)
 for resource in votable.resources:
@@ -17,7 +25,10 @@ for resource in votable.resources:
     for resource_meta in resource.resources:
         print(f"Resource type: {resource_meta.type} Mapping block:")
         print(resource_meta.model_mapping)
-        
+
+# Read an invalid VOTable   
+# An unexpected element has been found in the mappin block
+# The mapping block pointer returns a block with just REPORT in error     
 vpath = os.path.join(data_path, "test.7.ko.xml")
 votable = parse(vpath)
 for resource in votable.resources:
@@ -28,32 +39,33 @@ for resource in votable.resources:
         print(resource_meta.model_mapping)
  
  
-        
+"""
+Write Test
+"""       
 vpath = os.path.join(data_path, "test.7.out.xml")
 
+# Create am empty VOTable
 votable = VOTableFile()
+# Create the resource that will host both data table and mapping resource.
 resource = Resource()
 resource.type = "results"
+# Create the resource that will host the mapping.
 meta_resource = Resource()
 meta_resource.type = "meta"
-
+# A dummy mapping block for the test.
 resource.resources.append(meta_resource)
 model_mapping = ModelMapping("""
-<VODML>
-  <REPORT/>
-  <GLOBALS/>
-</VODML>
+<dm-mapping:VODML xmlns:dm-mapping="http://www.ivoa.net/xml/merged-syntax" >
+  <dm-mapping:REPORT/>
+  <dm-mapping:GLOBALS/>
+</dm-mapping:VODML>
 """
 )
-
-try:
-    resource.model_mapping = model_mapping
-except Exception as e:
-    print(e)
-
+# Add the mapping resource
 meta_resource.model_mapping = model_mapping
 votable.resources.append(resource)
-
+# Save the VOTable
 votable.to_xml(vpath)
+# and read it again to retrieve the mapping 
 with open(vpath) as result:
     print(result.read())
